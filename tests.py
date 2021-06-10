b2087054c37631d147c4b262f26a35fb44dfe153
USER <ivanenkogrig@yandex.ru>
2021-06-11 02:06:13 +0500

    update tests & upload tests using AioHTTPTestCase

--- tests.py
+++ tests.py
@@ -1,33 +1,68 @@
-import unittest
-import requests
+import json
+from aiohttp import web
+from aiohttp.test_utils import AioHTTPTestCase, unittest_run_loop
+
+from api import check_ip_validity, check_ports_validity, scan_ports
 
 
 def parametrize(*parameters):
     def decorator(func):
         def wrapper(*args, **kwargs):
             for params in parameters:
-                # print(f'Called function [{func.__name__}] '
-                #       f'with parameters={params}')
                 func(*args, *params)
-                # print(f'Test for [{func.__name__}] '
-                #       f'with parameters={params} passed!')
         return wrapper
     return decorator
 
 
-def _send_request(begin_port, end_port):
-    return requests.get(f'http://localhost:9090/scan/127.0.0.1/'
-                        f'{begin_port}/{end_port}')
+async def get_response_json(response):
+    response_json = await response.__dict__['content'].read()
+    return json.loads(response_json.decode())
 
 
-class PortScannerAPITestCase(unittest.TestCase):
+class MyTestAioHTTPTest(AioHTTPTestCase):
+
+    async def get_application(self):
+        async def handle_request(request: web.Request) -> web.Response:
+            try:
+                ip = request.match_info.get('ip')
+                begin_port = int(request.match_info.get('begin_port'))
+                end_port = int(request.match_info.get('end_port'))
+
+                if not check_ip_validity(ip):
+                    return web.HTTPBadRequest(
+                        text='400: Bad Request\n'
+                             'Invalid ip address!'
+                    )
+                if not check_ports_validity(begin_port, end_port):
+                    return web.HTTPBadRequest(
+                        text='400: Bad RequestInvalid ports!'
+                    )
+
+            except ValueError:
+                return web.HTTPBadRequest()
+
+            scan_results = scan_ports(ip, begin_port, end_port)
+
+            return web.Response(
+                text=json.dumps(scan_results)
+            )
+
+        app = web.Application()
+        app.router.add_routes(
+            [web.get('/scan/{ip}/{begin_port}/{end_port}', handle_request)
+             ])
+
+        return app
+
     @parametrize(
         [10, 20],
-        [68, 90],
-        [100, 150]
+        [68, 90]
     )
-    def test_response_format(self, begin_port, end_port):
-        response_json = _send_request(begin_port, end_port).json()
+    @unittest_run_loop
+    async def test_response_format(self, begin_port, end_port):
+        response = await self.client.request(
+            'GET', f'/scan/127.0.0.1/{begin_port}/{end_port}')
+        response_json = await get_response_json(response)
 
         self.assertIsInstance(response_json, list)
         self.assertEqual(end_port - begin_port + 1, len(response_json))
@@ -41,23 +76,26 @@ class PortScannerAPITestCase(unittest.TestCase):
 
     @parametrize(
         [10, 20],
-        [68, 90],
-        [100, 150]
+        [68, 90]
     )
-    def test_response_after_time(self, begin_port, end_port):
-        response_json_1 = _send_request(begin_port, end_port).json()
-        response_json_2 = _send_request(begin_port, end_port).json()
+    @unittest_run_loop
+    async def test_response_after_time(self, begin_port, end_port):
+        response_1 = await self.client.request(
+            'GET', f'/scan/127.0.0.1/{begin_port}/{end_port}')
+        response_json_1 = await get_response_json(response_1)
+
+        response_2 = await self.client.request(
+            'GET', f'/scan/127.0.0.1/{begin_port}/{end_port}')
+        response_json_2 = await get_response_json(response_2)
 
         self.assertEqual(response_json_1, response_json_2)
 
     @parametrize(
         ['sdfg', 20],
         [90, 68])
-    def test_response_for_incorrect_request(self, begin_port, end_port):
-        response = _send_request(begin_port, end_port)
+    @unittest_run_loop
+    async def test_response_for_incorrect_request(self, begin_port, end_port):
+        response = await self.client.request(
+            'GET', f'/scan/127.0.0.1/{begin_port}/{end_port}')
 
-        self.assertEqual(400, response.status_code)
-
-
-if __name__ == '__main__':
-    unittest.main()
+        self.assertEqual(400, response.status)
